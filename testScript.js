34e74c0d034ac804f04a6374b7196aca32ceea78
kushalv238 <kushalv238@gmail.com>
2024-03-06 21:04:19 +0530

    test extension code

---
+++ testScript.js
@@ -0,0 +1,25 @@
+BACKEND_URI = `https://gorest.co.in/public/v2/users`
+
+async function get() {
+    const url = BACKEND_URI;
+    const options = {
+        method: 'GET',
+        headers: {}
+    };
+
+    let result;
+
+    try {
+        const response = await fetch(url, options);
+        result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+
+    document.getElementById('editor-res').innerHTML = result.map((item, idx) => {
+        return `<li>${item.name}</li>`
+    }).join('')
+}
+
+get()
